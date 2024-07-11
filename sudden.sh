b9280dc61b09e45ee17f426bf6d7b41e3ac0ef47
Seokxkyu <kyuseok000@gmail.com>
2024-07-11 11:28:14 +0900

    date issue

--- sudden.sh
+++ sudden.sh
@@ -3,14 +3,16 @@
 figlet KIA
 
 FILE=~/tmp/_SUDDEN
+# DATE=$(date +"%Y%d%m %H:%M:%S")
 
 while true
+	DATE=$(date +"%Y%d%m %H:%M:%S")
 do 
 	if [[ -f "$FILE" ]]; then
        	   # echo "check $FILE"
 	   echo "SUDDEN!!!"
    	else
-	   date 
+	   echo $DATE
 	fi
 	sleep 3
 done
