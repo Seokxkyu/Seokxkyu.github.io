9f71677e259e4838c770675e5760dd91f8a0f4af
Seokxkyu <kyuseok000@gmail.com>
2024-07-11 11:45:13 +0900

    modify else

--- sudden.sh
+++ sudden.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-figlet KIA
-
 FILE=~/tmp/_SUDDEN
 # DATE=$(date +"%Y%d%m %H:%M:%S")
 
 while true
-	DATE=$(date +"%Y%d%m %H:%M:%S")
 do 
 	if [[ -f "$FILE" ]]; then
        	   # echo "check $FILE"
 	   echo "SUDDEN!!!"
    	else
+	   clear 
+	   figlet KIA
+	   DATE=$(date +"%Y%d%m %H:%M:%S")
 	   echo $DATE
 	fi
 	sleep 3
